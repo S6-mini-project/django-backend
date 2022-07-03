e9f3191ef3125da5cc0f95c2414342c265074409
Amar1013 <amarjithraj67@gmail.com>
2022-07-03 17:16:30 +0530

    reverted back to a commit to fix errors

--- app_backend/settings.py
+++ app_backend/settings.py
@@ -77,11 +77,14 @@ WSGI_APPLICATION = 'app_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'medpharma',
+        'USER':'root',
+        'PASSWORD':'',
+        'HOST':'localhost',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
